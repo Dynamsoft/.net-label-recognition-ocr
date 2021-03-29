24de58d29cefadf8437cc281af5edec9379caa55
xulihang <xulihanghai@163.com>
2021-03-29 17:52:02 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+using System;
+using System.ComponentModel;
+
+namespace BarcodeAndLabelRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ReadBarcodesButton = new System.Windows.Forms.Button();
+            this.ReadLabelsButton = new System.Windows.Forms.Button();
+            this.ReadLabelsWithDBRButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.DBRTemplateTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DLRTemplateTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(521, 565);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(577, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(315, 195);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ReadBarcodesButton
+            // 
+            this.ReadBarcodesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReadBarcodesButton.Location = new System.Drawing.Point(941, 28);
+            this.ReadBarcodesButton.Name = "ReadBarcodesButton";
+            this.ReadBarcodesButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ReadBarcodesButton.Size = new System.Drawing.Size(89, 23);
+            this.ReadBarcodesButton.TabIndex = 3;
+            this.ReadBarcodesButton.Text = "Read Barcodes";
+            this.ReadBarcodesButton.UseVisualStyleBackColor = true;
+            this.ReadBarcodesButton.Click += new System.EventHandler(this.ReadBarcodesButton_Click);
+            // 
+            // ReadLabelsButton
+            // 
+            this.ReadLabelsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReadLabelsButton.Location = new System.Drawing.Point(941, 75);
+            this.ReadLabelsButton.Name = "ReadLabelsButton";
+            this.ReadLabelsButton.Size = new System.Drawing.Size(89, 23);
+            this.ReadLabelsButton.TabIndex = 4;
+            this.ReadLabelsButton.Text = "Read Labels";
+            this.ReadLabelsButton.UseVisualStyleBackColor = true;
+            this.ReadLabelsButton.Click += new System.EventHandler(this.ReadLabelsButton_Click);
+            // 
+            // ReadLabelsWithDBRButton
+            // 
+            this.ReadLabelsWithDBRButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReadLabelsWithDBRButton.Location = new System.Drawing.Point(941, 122);
+            this.ReadLabelsWithDBRButton.Name = "ReadLabelsWithDBRButton";
+            this.ReadLabelsWithDBRButton.Size = new System.Drawing.Size(89, 53);
+            this.ReadLabelsWithDBRButton.TabIndex = 5;
+            this.ReadLabelsWithDBRButton.Text = "Read Labels with the help of DBR results";
+            this.ReadLabelsWithDBRButton.UseVisualStyleBackColor = true;
+            this.ReadLabelsWithDBRButton.Click += new System.EventHandler(this.ReadLabelsWithDBRButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClearButton.Location = new System.Drawing.Point(941, 200);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(89, 23);
+            this.ClearButton.TabIndex = 6;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // DBRTemplateTextBox
+            // 
+            this.DBRTemplateTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.DBRTemplateTextBox.Location = new System.Drawing.Point(0, 0);
+            this.DBRTemplateTextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.DBRTemplateTextBox.Multiline = true;
+            this.DBRTemplateTextBox.Name = "DBRTemplateTextBox";
+            this.DBRTemplateTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DBRTemplateTextBox.Size = new System.Drawing.Size(346, 300);
+            this.DBRTemplateTextBox.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Number",
+            "Letter",
+            "NumberLetter",
+            "NumberUppercase"});
+            this.comboBox1.Location = new System.Drawing.Point(941, 275);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(941, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "DLR OCR Model:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(577, 256);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(350, 320);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.DBRTemplateTextBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(342, 294);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DBR Template";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DLRTemplateTextBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(342, 294);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "DLR Template";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DLRTemplateTextBox
+            // 
+            this.DLRTemplateTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.DLRTemplateTextBox.Location = new System.Drawing.Point(0, 0);
+            this.DLRTemplateTextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.DLRTemplateTextBox.Multiline = true;
+            this.DLRTemplateTextBox.Name = "DLRTemplateTextBox";
+            this.DLRTemplateTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DLRTemplateTextBox.Size = new System.Drawing.Size(346, 298);
+            this.DLRTemplateTextBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 618);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ReadLabelsWithDBRButton);
+            this.Controls.Add(this.ReadLabelsButton);
+            this.Controls.Add(this.ReadBarcodesButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ReadBarcodesButton;
+        private System.Windows.Forms.Button ReadLabelsButton;
+        private System.Windows.Forms.Button ReadLabelsWithDBRButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TextBox DBRTemplateTextBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox DLRTemplateTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dynamsoft;
+using Dynamsoft.DBR;
+using Dynamsoft.DLR;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BarcodeAndLabelRecognition
+{    
+    public partial class Form1 : Form
+    {
+        private BarcodeReader reader = null;
+        private LabelRecognition labelRecognition = null;
+        public Form1()
+        {
+            InitializeComponent();
+            reader = new BarcodeReader("t0068MgAAAKDLOibg4+wFsMoX3cusNQ6/0s9n1XgfD2KLr+Jws31Stp/iCiBs9zrQSB37zO6T3AsHMs9TrunVGkIrPu0JFAc=");
+            labelRecognition = new LabelRecognition();
+            labelRecognition.InitLicense("t0068MgAAAJ96WjrYtvjfkUWwA0Qm5mWoK+Ri+Npyyqvfw2H/dNP+cgj2N2GaZLaJ9w9K7kCra5DghpdXMYMj3t9jFbIDhxg=");
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult dr = dialog.ShowDialog();
+            String filename;
+            if (dr == DialogResult.OK)
+            {
+                filename = dialog.FileName;
+            }
+            else
+            {
+                return;
+            }
+            Console.WriteLine(filename);
+            pictureBox1.Image= Image.FromFile(filename);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Tag = filename;
+            
+            textBox1.Text = "";
+
+        }
+
+        private void draw(Image img,Point[] points)
+        {
+            Graphics g = Graphics.FromImage(img);
+            Pen p = new Pen(Color.Red,1);
+            g.DrawLine(p, points[0], points[1]);
+            g.DrawLine(p, points[1], points[2]);
+            g.DrawLine(p, points[2], points[3]);
+            g.DrawLine(p, points[3], points[0]);
+            g.Save();
+            pictureBox1.Image = img;
+        }
+
+        //Use this when the pixel format is wrong
+        private Image PixelFormatFixed(String filename)
+        {
+            // The original bitmap with the wrong pixel format. 
+            // You can check the pixel format with originalBmp.PixelFormat
+            Image img = Image.FromFile(filename);
+            Bitmap originalBmp = (Bitmap)img;
+            // Create a blank bitmap with the same dimensions
+            Bitmap tempBitmap = new Bitmap(originalBmp.Width, originalBmp.Height);
+            // From this bitmap, the graphics can be obtained, because it has the right PixelFormat
+            using (Graphics g = Graphics.FromImage(tempBitmap))
+            {
+                // Draw the original bitmap onto the graphics of the new bitmap
+                g.DrawImage(originalBmp, 0, 0);
+            }
+            // Use tempBitmap as you would have used originalBmp
+            return tempBitmap;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReadBarcodesButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("reading dbr");
+            if (pictureBox1.Tag == null)
+            {
+                return;
+            }
+            if (DBRTemplateTextBox.Text != "")
+            {
+                try
+                {
+                    String o = "";
+                    reader.InitRuntimeSettingsWithString(DBRTemplateTextBox.Text, EnumConflictMode.CM_OVERWRITE, errorMessage: out o);
+                    PublicRuntimeSettings rs = reader.GetRuntimeSettings();
+                    rs.ResultCoordinateType = EnumResultCoordinateType.RCT_PIXEL;
+                    reader.UpdateRuntimeSettings(rs);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            try
+            {
+                TextResult[] results = reader.DecodeFile(pictureBox1.Tag.ToString(),"");
+                string outputInfo = "Total barcodes found: " + results.Length.ToString();
+                Console.WriteLine(outputInfo);
+                for (int iIndex = 0; iIndex < results.Length; ++iIndex)
+                {
+                    int iBarcodeIndex = iIndex + 1;
+                    string builder = "Barcode " + iBarcodeIndex.ToString() + ":\r\n";
+                    TextResult result = results[iIndex];
+                    if (result.BarcodeFormat != 0)
+                    {
+                        builder += "    Type: " + result.BarcodeFormatString + "\r\n";
+                    }
+                    else
+                    {
+                        builder += "    Type: " + result.BarcodeFormatString_2 + "\r\n";
+                    }
+                    builder += "    Value: " + result.BarcodeText + "\r\n";
+                    draw(pictureBox1.Image,result.LocalizationResult.ResultPoints);
+                    Console.WriteLine(builder);
+                    textBox1.Text = textBox1.Text + builder;
+                    
+                }
+                textBox1.Tag = results;
+
+            }
+            catch (BarcodeReaderException exp)
+            {
+                Console.WriteLine(exp.Message);
+                textBox1.Text = exp.Message;
+            }
+        }
+
+        private void ReadLabelsButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Tag == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("reading");
+            try
+            {
+                labelRecognition.ResetRuntimeSettings();
+                String templateName = LoadDLRTemplate();
+                if (templateName == "")
+                {
+                    DLR_RuntimeSettings rs = labelRecognition.GetRuntimeSettings();                    
+                    rs.RegionPredetectionModes.Append(EnumDLRRegionPredetectionMode.DLR_RPM_GENERAL_GRAY_CONTRAST);
+                    //rs.RegionPredetectionModes.Append(EnumDLRRegionPredetectionMode.DLR_RPM_GENERAL_RGB_CONTRAST);
+
+                    rs.LinesCount = 100;
+                    rs.CharacterModelName = comboBox1.SelectedItem.ToString();
+                    labelRecognition.UpdateRuntimeSettings(rs);
+                }
+               
+                DLR_Result[] results = labelRecognition.RecognizeByFile(pictureBox1.Tag.ToString(), templateName);
+                OutputDLRResult(results);
+            }
+            catch (DLR_Exception exp)
+            {
+                Console.WriteLine(exp);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+            }
+        }
+
+        private void ReadLabelsWithDBRButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Tag == null)
+            {
+                return;
+            }
+            if (textBox1.Tag == null)
+            {
+                MessageBox.Show("Please read barcodes first.");
+
+                return;
+            }
+            TextResult[] dbr_result = (TextResult[])textBox1.Tag;
+            String templateName = LoadDLRTemplate();
+            if (templateName == "")
+            {
+                DLR_RuntimeSettings rs = labelRecognition.GetRuntimeSettings();
+                rs.ReferenceRegion.LocalizationSourceType = EnumDLRLocalizationSourceType.DLR_LST_BARCODE;
+                Console.WriteLine(rs.ReferenceRegion.Points.ToString());
+                rs.LinesCount = 100;
+                rs.CharacterModelName = comboBox1.SelectedItem.ToString();
+                labelRecognition.UpdateRuntimeSettings(rs);                
+            }
+
+            labelRecognition.UpdateReferenceRegionFromBarcodeResults(dbr_result, templateName);
+            DLR_Result[] results = labelRecognition.RecognizeByFile(pictureBox1.Tag.ToString(), templateName);
+            
+            //labelRecognition.OutputSettingsToFile("D:\\out.json", "");
+            OutputDLRResult(results);
+        }
+
+        private void OutputDLRResult(DLR_Result[] results)
+        {
+            string builder = "";
+            Console.WriteLine(results.Length);
+            for (int i = 0; i < results.Length; ++i)
+            {
+                
+                builder += "Result " + i.ToString() + ": \r\n";
+                for (int j = 0; j < results[i].LineResults.Length; ++j)
+                {
+                    builder += ">>LineResult " + j.ToString() + ": " + results[i].LineResults[j].Text + "\r\n";
+
+                    draw(pictureBox1.Image, results[i].LineResults[j].Location.Points);
+                }
+            }
+            textBox1.Text += builder;
+        }
+
+        private String LoadDLRTemplate()
+        {
+            labelRecognition.ClearAppendedSettings();
+            String templateName = "";
+            if (DLRTemplateTextBox.Text != "")
+            {
+                try
+                {
+                    labelRecognition.AppendSettingsFromString(DLRTemplateTextBox.Text);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Wrong json.");
+                    throw;
+                }
+
+                JObject jo = (JObject)JsonConvert.DeserializeObject(DLRTemplateTextBox.Text);
+                List<JToken> array = jo["LabelRecognitionParameterArray"].ToList<JToken>();
+                templateName = array[0]["Name"].ToString();
+                Console.WriteLine("added template: " + templateName);
+                return templateName;
+            }
+            return "";
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(pictureBox1.Tag.ToString());
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
